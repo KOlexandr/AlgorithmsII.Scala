a309ac4ebc51f5829ad533840431b3820598889c
Olexandr <kolexandr.13@gmail.com>
2014-10-01 01:29:01 +0300

    IntervalArithmetic

---
+++ src/main/scala/project/intervalArithmetic/IATest.scala
@@ -0,0 +1,38 @@
+import project.intervalArithmetic.Interval
+
+object IATest extends App {
+  val interval_0_1: Interval = new Interval(0, 1)
+  val interval_2_5: Interval = new Interval(2, 5)
+  val interval_05_15: Interval = new Interval(0.5, 1.5)
+  private val interval_15_2: Interval = new Interval(1.5, 2)
+
+  println(interval_0_1)
+
+  println(interval_0_1 + " + " + interval_2_5 + " = " + (interval_0_1 + interval_2_5) + "\n")
+
+  println(interval_0_1 + " - " + interval_2_5 + " = " + (interval_0_1 - interval_2_5) + "\n")
+
+  println(interval_15_2 + " * " + interval_2_5 + " = " + (interval_15_2 * interval_2_5) + "\n")
+
+  println(interval_05_15  + " / " + interval_2_5 + " = " + (interval_05_15 / interval_2_5) + "\n")
+
+  println(interval_2_5 + " == " + interval_2_5  + " = " + (interval_2_5 == interval_2_5) + "\n")
+
+  println(interval_05_15  + " == " + interval_2_5 + " = " + (interval_05_15 == interval_2_5) + "\n")
+
+  println(interval_05_15 + " != " + interval_2_5 + " = " + (interval_05_15 != interval_2_5) + "\n")
+
+  println(interval_2_5 + " != " + interval_2_5 + " = " + (interval_2_5 != interval_2_5) + "\n")
+
+  println(interval_2_5  + " < " + interval_2_5 + " = " + (interval_2_5 < interval_2_5) + "\n")
+
+  println(interval_2_5 + " <= " + interval_2_5 + " = " + (interval_2_5 <= interval_2_5) + "\n")
+
+  println(interval_2_5 + " > " + interval_2_5 + " = " + (interval_2_5 > interval_2_5) + "\n")
+
+  println(interval_2_5 + " >= " + interval_2_5 + " = " + (interval_2_5 >= interval_2_5) + "\n")
+
+  println("-" + interval_2_5 + " = " + (-interval_2_5) + "\n")
+
+  println(interval_0_1 + " intersection " + interval_05_15 + " = " + (interval_0_1 intersection  interval_05_15) + "\n")
+}
--- src/main/scala/project/intervalArithmetic/Interval.scala
+++ src/main/scala/project/intervalArithmetic/Interval.scala
@@ -16,23 +16,29 @@ class Interval(val left: Double, val right: Double) {
 
   def / (that: Interval) =
     if(that.left == 0 || that.right == 0) throw new IllegalArgumentException("Divider can not by 0")
-    else this * new Interval(1/that.left, 1/that.right)
+    else this * new Interval(math.min(1/that.left, 1/that.right), math.max(1/that.left, 1/that.right))
 
   def < (that: Interval) = this.right < that.left
 
   def > (that: Interval) = that < this
 
+  def <= (that: Interval) = this < that || this == that
+
+  def >= (that: Interval) = that <= this
+
   def == (that: Interval) = this.left == that.left && this.right == that.right
+
+  def != (that: Interval) = !(this == that)
 
   def unary_- = new Interval(math.min(-this.left, -this.right), math.max(-this.left, -this.right))
 
-  def ^ (that: Interval) =
+  def intersection (that: Interval) =
     if(this < that || that < this) null
     else new Interval(math.max(this.left, that.left), math.min(this.right, that.right))
 
   def width = right - left
 
-  def middle = (left + right) / 2
+  def median = (left + right) / 2
 
   def abs = math.max(math.abs(left), math.abs(right))
 
@@ -40,7 +46,9 @@ class Interval(val left: Double, val right: Double) {
 
   def isSymmetric = -left == right
 
-  def mu = math.min(math.abs(left), math.abs(right))
+  def inf = left
 
-  def s = (math.abs(left) + math.abs(right)) / 2
+  def sup = right
+
+  override def toString: String = "[" + left + ", " + right + "]"
 }
