8b38d82b8ae8644e56f0ec64ec917cc14966e5ac
Olexandr <kolexandr.13@gmail.com>
2014-12-18 00:09:55 +0200

    Computational Geometry

--- src/main/scala/lab12/Test.scala
+++ src/main/scala/lab12/Test.scala
@@ -8,7 +8,7 @@ object Test {
   }
 
   def segmentIntersect(): Unit = {
-    val line1 = L(0, 5, 4, 4)
+    val line1 = L(0, 5, 4, 4) // Figure 33.3 a) b)
     //    val line1 = Line(0, 5, 14, 3)
     val line2 = L(1, 1, 15, 8)
     if (SegmentIntersect.isIntersect(line1, line2)) {
@@ -19,7 +19,7 @@ object Test {
   }
 
   def anySegmentsIntersect(): Unit = {
-    val lines = Array(
+    val lines = Array(    // Figure 33.5
       L(5, 25, 25, 31),   //a
       L(10, 3, 80, 30),   //b
       L(15, 12, 35, 18),  //c
